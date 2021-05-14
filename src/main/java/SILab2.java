fae50ddf992b1db8143e4eced050064b51c31a3e
borcesmileski <borce.smileski@gmail.com>
2021-05-14 03:33:47 +0200

    tests

--- src/main/java/SILab2.java
+++ src/main/java/SILab2.java
@@ -5,21 +5,17 @@ class Time {
     int hours;
     int minutes;
     int seconds;
-
     public Time(int hours, int minutes, int seconds) {
         this.hours = hours;
         this.minutes = minutes;
         this.seconds = seconds;
     }
-
     public int getHours() {
         return hours;
     }
-
     public int getMinutes() {
         return minutes;
     }
-
     public int getSeconds() {
         return seconds;
     }
--- src/test/java/SILab2Test.java
+++ src/test/java/SILab2Test.java
@@ -5,31 +5,242 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class SILab2Test {
-
+class EveryBranch {
+    //1,2,3,4,8
     @Test
-    public void function() {
-        //Testing branch 1,2,3,4,5,8
-        Time angle = new Time(10, 30, 30);
-        List<Time> anglesList = new ArrayList<>();
-        anglesList.add(angle);
+    public void test1() {
+        Time t = new Time(-11, 30, 30);
+        List<Time> lists = new ArrayList<>();
+        lists.add(t);
         try {
-            SILab2.function(anglesList);
+            SILab2.function(lists);
         } catch (Exception e) {
-            assertEquals(e.getMessage(), "The minutes of the angle are not valid!");
+            assertEquals(e.getMessage(), "The hours are smaller than the minimum");
         }
     }
+    //1,2,3,4,9
     @Test
-    public void function1() {
+    public void test2() {
         //Testing branch 1,2,3,4,5,8
-        Time angle = new Time(10, 30, 30);
-        List<Time> anglesList = new ArrayList<>();
-        anglesList.add(angle);
+        Time t = new Time(34, 30, 30);
+        List<Time> lists = new ArrayList<>();
+        lists.add(t);
         try {
-            SILab2.function(anglesList);
+            SILab2.function(lists);
         } catch (Exception e) {
-            assertEquals(e.getMessage(), "The minutes of the angle are not valid!");
+            assertEquals(e.getMessage(), "The hours are grater than the maximum");
+        }
+    }
+    //1,2,3,5,10
+    @Test
+    public void test3() {
+        //Testing branch 1,2,3,4,5,8
+        Time t = new Time(14, 74, 30);
+        List<Time> lists = new ArrayList<>();
+        lists.add(t);
+        try {
+            SILab2.function(lists);
+        } catch (Exception e) {
+            assertEquals(e.getMessage(), "The minutes are not valid!");
+        }
+    }
+    //1,2,3,5,11,12
+    @Test
+    public void test4() {
+        Time t = new Time(15, 26, 52);
+        List<Time> list = new ArrayList<>();
+        List<Integer> result = new ArrayList<>();
+        list.add(t);
+        result = SILab2.function(list);
+        assertEquals(result.get(0).intValue(), t.getHours() * 3600 + t.getMinutes() * 60 + t.getSeconds());
+    }
+    //1,2,3,5,11,13
+    @Test
+    public void test5() {
+        //Testing branch 1,2,3,4,5,8
+        Time t = new Time(14, 54, 70);
+        List<Time> lists = new ArrayList<>();
+        lists.add(t);
+        try {
+            SILab2.function(lists);
+        } catch (Exception e) {
+            assertEquals(e.getMessage(), "The seconds are not valid");
+        }
+    }
+    //1,2,3,6
+    @Test
+    public void test6() {
+        Time t = new Time(24, 0, 0);
+        List<Time> list = new ArrayList<>();
+        List<Integer> result = new ArrayList<>();
+        list.add(t);
+        result = SILab2.function(list);
+        assertEquals(result.get(0).intValue(), t.getHours() * 3600 + t.getMinutes() * 60 + t.getSeconds());
+    }
+    //1,2,3,7
+    @Test
+    public void test7() {
+        Time t = new Time(24, 0, 2);
+        List<Time> lists = new ArrayList<>();
+        lists.add(t);
+        try {
+            SILab2.function(lists);
+        } catch (Exception e) {
+            assertEquals(e.getMessage(), "The time is greater than the maximum");
+        }
+    }
+}
+class MultipleCondition {
+    //1,2,3,4,8
+    @Test
+    public void test1() {
+        Time t = new Time(-11, 30, 30);
+        List<Time> lists = new ArrayList<>();
+        lists.add(t);
+        try {
+            SILab2.function(lists);
+        } catch (Exception e) {
+            assertEquals(e.getMessage(), "The hours are smaller than the minimum");
+        }
+    }
+    //1,2,3,4,9
+    @Test
+    public void test2() {
+        Time t = new Time(56, 30, 30);
+        List<Time> lists = new ArrayList<>();
+        lists.add(t);
+        try {
+            SILab2.function(lists);
+        } catch (Exception e) {
+            assertEquals(e.getMessage(), "The hours are grater than the maximum");
+        }
+    }
+    //1,2,3,6
+    @Test
+    public void test3() {
+        Time t = new Time(14, 0, 0);
+        List<Time> list = new ArrayList<>();
+        List<Integer> result = new ArrayList<>();
+        list.add(t);
+        result = SILab2.function(list);
+        assertEquals(result.get(0).intValue(), t.getHours() * 3600 + t.getMinutes() * 60 + t.getSeconds());
+    }
+    //1,2,3,5,10 F || X
+    @Test
+    public void test4() {
+        Time t = new Time(14, -23, 30);
+        List<Time> lists = new ArrayList<>();
+        lists.add(t);
+        try {
+            SILab2.function(lists);
+        } catch (Exception e) {
+            assertEquals(e.getMessage(), "The minutes are not valid!");
+        }
+    }
+    //1,2,3,5,10 X || F
+    @Test
+    public void test5() {
+        Time t = new Time(14, 73, 30);
+        List<Time> lists = new ArrayList<>();
+        lists.add(t);
+        try {
+            SILab2.function(lists);
+        } catch (Exception e) {
+            assertEquals(e.getMessage(), "The minutes are not valid!");
+        }
+    }
+    //1,2,3,5,11,12 F || F
+    @Test
+    public void test6() {
+        Time t = new Time(15, 26, 52);
+        List<Time> list = new ArrayList<>();
+        List<Integer> result = new ArrayList<>();
+        list.add(t);
+        result = SILab2.function(list);
+        assertEquals(result.get(0).intValue(), t.getHours() * 3600 + t.getMinutes() * 60 + t.getSeconds());
+    }
+    //1,2,3,5,11,12 T && T
+    @Test
+    public void test7() {
+        Time t = new Time(15, 26, 52);
+        List<Time> list = new ArrayList<>();
+        List<Integer> result = new ArrayList<>();
+        list.add(t);
+        result = SILab2.function(list);
+        assertEquals(result.get(0).intValue(), t.getHours() * 3600 + t.getMinutes() * 60 + t.getSeconds());
+    }
+    //1,2,3,5,11,13 F && T
+    @Test
+    public void test8() {
+        //Testing branch 1,2,3,4,5,8
+        Time t = new Time(14, 54, -40);
+        List<Time> lists = new ArrayList<>();
+        lists.add(t);
+        try {
+            SILab2.function(lists);
+        } catch (Exception e) {
+            assertEquals(e.getMessage(), "The seconds are not valid");
+        }
+    }
+    //1,2,3,5,11,13 T && F
+    @Test
+    public void test9() {
+        //Testing branch 1,2,3,4,5,8
+        Time t = new Time(14, 54, 90);
+        List<Time> lists = new ArrayList<>();
+        lists.add(t);
+        try {
+            SILab2.function(lists);
+        } catch (Exception e) {
+            assertEquals(e.getMessage(), "The seconds are not valid");
+        }
+    }
+    //1,2,3,6 T && T && T
+    @Test
+    public void test10() {
+        Time t = new Time(24, 0, 0);
+        List<Time> list = new ArrayList<>();
+        List<Integer> result = new ArrayList<>();
+        list.add(t);
+        result = SILab2.function(list);
+        assertEquals(result.get(0).intValue(), t.getHours() * 3600 + t.getMinutes() * 60 + t.getSeconds());
+    }
+    //1,2,3,4,8  F && T && T
+    @Test
+    public void test11() {
+        Time t = new Time(-11, 0, 0);
+        List<Time> lists = new ArrayList<>();
+        lists.add(t);
+        try {
+            SILab2.function(lists);
+        } catch (Exception e) {
+            assertEquals(e.getMessage(), "The hours are smaller than the minimum");
+        }
+    }
+    //1,2,3,7  T && F && T
+    @Test
+    public void test12() {
+        Time t = new Time(24, 5, 0);
+        List<Time> lists = new ArrayList<>();
+        lists.add(t);
+        try {
+            SILab2.function(lists);
+        } catch (Exception e) {
+            assertEquals(e.getMessage(), "The time is greater than the maximum");
+        }
+    }
+    //1,2,3,7  T && T && F
+    @Test
+    public void test13() {
+        Time t = new Time(24, 0, 42);
+        List<Time> lists = new ArrayList<>();
+        lists.add(t);
+        try {
+            SILab2.function(lists);
+        } catch (Exception e) {
+            assertEquals(e.getMessage(), "The time is greater than the maximum");
         }
     }
 
 }
+
